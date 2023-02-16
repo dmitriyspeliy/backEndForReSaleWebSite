914125e6cb8687b3da98d4b17db36adcbcf27b10
Igor <47169805+IgorDevR@users.noreply.github.com>
2023-02-16 22:07:26 +0600

    add adduser

--- src/main/java/ru/skypro/homework/service/UserService.java
+++ src/main/java/ru/skypro/homework/service/UserService.java
@@ -3,6 +3,8 @@ package ru.skypro.homework.service;
 import org.springframework.security.core.Authentication;
 import org.springframework.web.multipart.MultipartFile;
 import ru.skypro.homework.dto.NewPassword;
+import ru.skypro.homework.dto.RegisterReq;
+import ru.skypro.homework.dto.Role;
 import ru.skypro.homework.dto.UserDTO;
 
 /**
@@ -14,6 +16,10 @@ public interface UserService {
    * получить пользователя
    */
   UserDTO getUser(Authentication authentication);
+  /**
+   * получить пользователя
+   */
+  UserDTO addUser(RegisterReq registerReq, Role role);
 
   /**
    * обновить пользователя
