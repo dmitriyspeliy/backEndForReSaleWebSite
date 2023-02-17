7726bcfbd23bf42f7de17f2955b89abf7fed711f
AntonGraf <ant0ngraf@yandex.ru>
2023-02-17 15:52:58 +0600

    Добавляет маппер toAdsDTO

---
+++ src/main/java/ru/skypro/homework/mapper/AdOtherMapper.java
@@ -0,0 +1,15 @@
+package ru.skypro.homework.mapper;
+
+import org.mapstruct.Mapper;
+import org.mapstruct.Mapping;
+import ru.skypro.homework.dto.AdsDTO;
+import ru.skypro.homework.entity.AdEntity;
+
+@Mapper(uses = {ImageMapper.class}, componentModel = "spring")
+public interface AdOtherMapper {
+
+    @Mapping(target = "author", source = "author.id")
+    @Mapping(target = "pk", source = "id")
+    @Mapping(target = "image", source = "imageEntities")
+    AdsDTO toAdsDTO(AdEntity adEntity);
+}
---
+++ src/test/java/ru/skypro/homework/mapper/AdOtherMapperTest.java
@@ -0,0 +1,77 @@
+package ru.skypro.homework.mapper;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import ru.skypro.homework.dto.AdsDTO;
+import ru.skypro.homework.entity.AdEntity;
+import ru.skypro.homework.entity.ImageEntity;
+import ru.skypro.homework.entity.UserEntity;
+
+import java.time.LocalDateTime;
+import java.util.ArrayList;
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+@SpringBootTest
+class AdOtherMapperTest {
+
+    @Autowired
+    private AdOtherMapper adOtherMapper;
+
+    @Test
+    void toAdsDTO() {
+        AdEntity adEntity = new AdEntity();
+        adEntity.setId(1);
+        adEntity.setPrice(999);
+        adEntity.setTitle("Title");
+        adEntity.setDescription("Description");
+        adEntity.setCommentEntities(List.of());
+        adEntity.setAuthor(getAuthor());
+        adEntity.setImageEntities(getImageEntities());
+
+        AdsDTO exceptedDto = new AdsDTO();
+        exceptedDto.setPrice(999);
+        exceptedDto.setPk(1);
+        exceptedDto.setTitle("Title");
+        exceptedDto.setAuthor(2);
+        exceptedDto.setImage(getImagePathString());
+
+        assertEquals(exceptedDto, adOtherMapper.toAdsDTO(adEntity));
+    }
+
+    private UserEntity getAuthor() {
+        UserEntity userEntity = new UserEntity();
+        userEntity.setId(2);
+        userEntity.setRegDate(LocalDateTime.of(2023, 02, 16, 14,30, 22));
+        userEntity.setPhone("7884643");
+        userEntity.setEmail("asda@asd.re");
+        userEntity.setCity("MSK");
+        userEntity.setFirstName("Иван");
+        userEntity.setLastName("Иванов");
+        userEntity.setImage("/user/image/1");
+        return userEntity;
+    }
+
+    private List<ImageEntity> getImageEntities() {
+        List<ImageEntity> imageEntities = new ArrayList<>();
+        for (int i = 1; i < 5; i++) {
+            ImageEntity imageEntity = new ImageEntity();
+            imageEntity.setId(i);;
+            imageEntity.setPath("/ads/images/" + i);
+            imageEntities.add(imageEntity);
+        }
+
+        return imageEntities;
+    }
+
+    private List<String> getImagePathString() {
+        List<String> imagePathStrings = new ArrayList<>();
+        imagePathStrings.add("/ads/images/" + 1);
+        imagePathStrings.add("/ads/images/" + 2);
+        imagePathStrings.add("/ads/images/" + 3);
+        imagePathStrings.add("/ads/images/" + 4);
+        return imagePathStrings;
+    }
+}
